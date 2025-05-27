885bd53762557873ad7f2efcec39a11e5127db0a
Tony Wang <twang849@uwo.ca>
2025-05-27 14:09:50 -0400

    lock fix

--- src/app/ui/LinkButton.tsx
+++ src/app/ui/LinkButton.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function LinkButton ({ link, src, alt }: {link: string, src: string, alt: string}) {
     return (
-        <a href={link} target="_blank" rel="noopener noreferrer" className= "hover:opacity-50 ease-out transition duration-300">
+        <a href={link} target="_blank" rel="noopener noreferrer" className= "hover:opacity-50 ease-out transition duration-200">
             <Image
                 src={src}
                 width={50}
--- src/app/ui/Title.tsx
+++ src/app/ui/Title.tsx
@@ -5,7 +5,7 @@ const playfair_display = Playfair_Display({subsets: ['latin']});
 export default function Title () {
     return (
         <div id="title" className={`${playfair_display.className} select-none z-10  text-[10rem] leading-none`}>
-            <div className="hover:text-gray-500 text-shadow-lg transition duration-300 ease-out text-white text-center">
+            <div className="hover:text-gray-950 text-shadow-lg transition duration-300 ease-out text-white text-center">
                 KATELYN <br/>
                 GIRODAT
             </div>
